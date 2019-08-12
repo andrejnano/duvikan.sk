b1c67cc338167b802975966161acce51d01707c2
xnanoa00@stud.fit.vutbr.cz <xnanoa00@stud.fit.vutbr.cz>
2019-08-12 17:03:09 +0200

    fix seo settings

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,8 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { HelmetDatoCms } from 'gatsby-source-datocms';
 
+// todo: add dynamic lang attr setup
 const SEO = ({ meta }) => {
-  return <HelmetDatoCms seo={meta} />;
+  return (
+    <HelmetDatoCms seo={meta}>
+      <html lang="sk" />
+    </HelmetDatoCms>
+  );
 };
 
 SEO.propTypes = {
--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { graphql, StaticQuery } from 'gatsby';
-import BackgroundImage from 'gatsby-background-image';
-
 import Transition from '../components/Transition';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -47,61 +45,6 @@ const SiteWrapper = styled.div`
   background-color: ${colors.mediumWash};
 `;
 
-// /**
-//  * This component wraps all other page components in a single block.
-//  * @param {children} objects all underlying components
-//  * @param {className} class property passed in from parent
-//  * @param {homePageTest} boolean to test if the current page is Homepage, so backgroundImage component will be used
-//  */
-// const SiteWrapper = ({ children, className, homePageTest }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query {
-//         homePage: datoCmsHomePage {
-//           backgroundImage {
-//             fluid(maxWidth: 4160) {
-//               ...GatsbyDatoCmsFluid
-//             }
-//           }
-//         }
-//       }
-//     `}
-//     render={data => {
-//       const imageData = data.homePage.backgroundImage.fluid;
-//       if (homePageTest) {
-//         // if the page is homepage, display fluid image component in the background
-//         return (
-//           <BackgroundImage
-//             Tag="section"
-//             className={className}
-//             fluid={imageData}
-//             backgroundColor={colors.mediumWash}
-//           >
-//             {children}
-//           </BackgroundImage>
-//         );
-//       } else {
-//         // for all other pages, use regular site wrapper with single color background
-//         return <RegularSiteWrapper>{children}</RegularSiteWrapper>;
-//       }
-//     }}
-//   />
-// );
-
-// const StyledSiteWrapper = styled(SiteWrapper)`
-//   width: 100%;
-//   min-height: 100vh;
-//   background-size: auto;
-//   background-color: transparent;
-//   background-repeat: no-repeat, no-repeat, repeat;
-//   &.showImageBg {
-//     background-origin: content-box;
-//     background-position: center top;
-//     background-repeat: no-repeat;
-//     background-size: 300px 100px;
-//   }
-// `;
-
 const ContentWrapper = styled.main`
   min-height: calc(100vh - (54px + 2rem));
   margin-top: calc(54px + 2rem);
@@ -119,9 +62,10 @@ const Layout = ({ children, location }) => {
       <Reboot />
       <Global />
       <Header isHome={isHome} />
-      <Transition location={location}>
+      {/* <Transition location={location}>
         <ContentWrapper>{children}</ContentWrapper>
-      </Transition>
+      </Transition> */}
+      <ContentWrapper>{children}</ContentWrapper>
       <Footer />
     </SiteWrapper>
   );
